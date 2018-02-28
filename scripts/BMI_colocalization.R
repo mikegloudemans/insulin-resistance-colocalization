e83fce74ad05bfb7c91b86e08580463b39856f14
mikegloudemans <mgloud@durga.stanford.edu>
2018-02-27 17:14:24 -0800

    Update

--- scripts/BMI_colocalization.R
+++ scripts/BMI_colocalization.R
@@ -15,10 +15,11 @@ finemap = read_delim("/users/mgloud/projects/brain_gwas/output/2018-01-28_08-55-
 finemap$chrom = sapply(finemap$snp, function(x){as.numeric(strsplit(x, "_")[[1]][1])})
 finemap$pos = sapply(finemap$snp, function(x){as.numeric(strsplit(x, "_")[[1]][2])})
 
-
-all_coloc_scores = array(0, c(dim(hits)[1], length(tissues)))
+# Since we don't know the target size, just make it larger than needed
+all_coloc_scores = array(0, c(1000, length(tissues)))
 colnames(all_coloc_scores) = tissues
-rownames(all_coloc_scores) = paste(hits$chr, "_", hits$snp_pos, sep="")
+rownames(all_coloc_scores) = 1:1000
+coloc_num = 0
 for (i in 1:dim(hits)[1])
 {
 	hit = hits[i,]
@@ -28,40 +29,56 @@ for (i in 1:dim(hits)[1])
 	print(i)
 
 	if (dim(hit_coloc)[1] == 0)
-	{
+	{ 	
+		coloc_num = coloc_num + 1
+		rownames(all_coloc_scores)[coloc_num] = paste(hit$chr, hit$snp_pos, sep="_")
+		all_coloc_scores[coloc_num,] = rep(NA, length(tissues))
 		next
 	}
 	print(max(hit_coloc$clpp))	
 
-	# Figure out which gene have putative colocalization in any tissue
-	best_gene = hit_coloc$gene[which.max(hit_coloc$clpp)]
-	print(best_gene)
-
-	rownames(all_coloc_scores)[i] = paste(rownames(all_coloc_scores)[i], "_", best_gene, sep="")
-	
-
-	gene_coloc = hit_coloc[hit_coloc$gene == best_gene,]
-
-	# See to what extent that gene colocalizes in each tissue, and store these
-	# results in a matrix for heatmap plotting.
-	
-	for (j in 1:length(tissues))
+	# Figure out which genes have putative colocalization in any tissue
+	best_genes = unique(hit_coloc$gene[hit_coloc$clpp > 0.01])
+	if (length(best_genes) == 0)
 	{
-		tissue = paste(tissues[j], "_allpairs_txt_gz", sep="")
-		tissue_coloc = gene_coloc[gene_coloc$eqtl_file == tissue,]
-		if (dim(tissue_coloc)[1] == 0)
+		best_genes = hit_coloc$gene[which.max(hit_coloc$clpp)]
+	}
+	print(best_genes)
+
+	for (bg in best_genes)
+	{
+		coloc_num = coloc_num + 1
+		rownames(all_coloc_scores)[coloc_num] = paste(hit$chr, hit$snp_pos, bg, sep="_")	
+
+		# Get an unfiltered version of the hits so that we we can see scores for 
+		# genes that are just barely sub-threshold
+		hit_unfiltered_coloc = finemap[finemap$chrom == hit$chr & abs(finemap$pos - hit$snp_pos) <= 500000,]
+		gene_coloc = hit_unfiltered_coloc[hit_unfiltered_coloc$gene == bg,]
+
+		# See to what extent that gene colocalizes in each tissue, and store these
+		# results in a matrix for heatmap plotting.
+		
+		for (j in 1:length(tissues))
 		{
-			all_coloc_scores[i,j] = 0
-		}
-		else
-		{
-			# Sanity check: we should only have one score left
-			# at this point
-			stopifnot(dim(tissue_coloc)[1] == 1)
-			all_coloc_scores[i,j] = tissue_coloc$clpp
+			tissue = paste(tissues[j], "_allpairs_txt_gz", sep="")
+			tissue_coloc = gene_coloc[gene_coloc$eqtl_file == tissue,]
+			if (dim(tissue_coloc)[1] == 0)
+			{
+				all_coloc_scores[coloc_num,j] = NA
+			}
+			else
+			{
+				# Sanity check: we should only have one score left
+				# at this point
+				stopifnot(dim(tissue_coloc)[1] == 1)
+				all_coloc_scores[coloc_num,j] = tissue_coloc$clpp
+			}
 		}
 	}
 }
+
+# Discard extra allocated rows
+all_coloc_scores = all_coloc_scores[1:(coloc_num-1),]
 
 # Bin scores to make heatmap look nicer
 binned_coloc_scores = cut(all_coloc_scores, breaks=c(-0.001, 0.005, 0.01, 0.02, 0.1, 1), labels=1:5)
@@ -83,7 +100,7 @@ fix$gene = rownames(fix)
 heat = melt(fix, id="gene")
 g = ggplot(data = heat, aes(x = variable, y = gene)) +
 		geom_tile(aes(fill = value)) +
-		scale_fill_manual(drop=FALSE, values=colorRampPalette(c("white","red"))(5), name="CLPP", labels = c("0-0.005", "0.005-0.01", "0.01-0.02", "0.02-0.1", "0.1-1")) +
+		scale_fill_manual(drop=FALSE, values=colorRampPalette(c("white","red"))(5), name="CLPP", labels = c("0-0.005", "0.005-0.01", "0.01-0.02", "0.02-0.1", "0.1-1"),na.value = "grey80") +
 		theme(axis.text.y=element_text(size=7),
 		      axis.text.x = element_text(angle = 45, hjust = 1)) +
                 labs(x = "Tissue") +
