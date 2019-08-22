4d4f17b1478c2fade4874a7b483f6c48c7a05bad
mikegloudemans <mgloud@durga.stanford.edu>
2019-08-22 16:13:05 -0700

    A few more changes to munge config files

--- scripts/format_gwas/munge.sh
+++ scripts/format_gwas/munge.sh
@@ -1,1 +1,1 @@
-python /users/mgloud/projects/gwas-download/scripts/custom_munge.py munge_all.config
+python /users/mgloud/projects/gwas-download/munge/custom_munge.py /users/mgloud/projects/insulin_resistance/scripts/format_gwas/munge_all.config
