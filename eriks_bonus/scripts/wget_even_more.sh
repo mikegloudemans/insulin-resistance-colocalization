1409f2f42e80394eaf9be396a5ba071673d222f8
mikegloudemans <mgloud@durga.stanford.edu>
2019-08-22 11:20:00 -0700

    Added a bunch of un-versioned files

---
+++ eriks_bonus/scripts/wget_even_more.sh
@@ -0,0 +1,11 @@
+mkdir -p /users/mgloud/projects/insulin_resistance/eriks_bonus/data/Pulit-et-al-2018
+
+wget https://zenodo.org/record/1251813/files/bmi.giant-ukbb.meta-analysis.combined.23May2018.txt.gz -P /users/mgloud/projects/insulin_resistance/eriks_bonus/data/Pulit-et-al-2018
+wget https://zenodo.org/record/1251813/files/bmi.giant-ukbb.meta-analysis.females.23May2018.txt.gz -P /users/mgloud/projects/insulin_resistance/eriks_bonus/data/Pulit-et-al-2018
+wget https://zenodo.org/record/1251813/files/bmi.giant-ukbb.meta-analysis.males.23May2018.txt.gz -P /users/mgloud/projects/insulin_resistance/eriks_bonus/data/Pulit-et-al-2018
+wget https://zenodo.org/record/1251813/files/whr.giant-ukbb.meta-analysis.combined.23May2018.txt.gz -P /users/mgloud/projects/insulin_resistance/eriks_bonus/data/Pulit-et-al-2018
+wget https://zenodo.org/record/1251813/files/whr.giant-ukbb.meta-analysis.females.23May2018.txt.gz -P /users/mgloud/projects/insulin_resistance/eriks_bonus/data/Pulit-et-al-2018
+wget https://zenodo.org/record/1251813/files/whr.giant-ukbb.meta-analysis.males.23May2018.txt.gz -P /users/mgloud/projects/insulin_resistance/eriks_bonus/data/Pulit-et-al-2018
+wget https://zenodo.org/record/1251813/files/whradjbmi.giant-ukbb.meta-analysis.combined.23May2018.txt.gz -P /users/mgloud/projects/insulin_resistance/eriks_bonus/data/Pulit-et-al-2018
+wget https://zenodo.org/record/1251813/files/whradjbmi.giant-ukbb.meta-analysis.females.23May2018.txt.gz -P /users/mgloud/projects/insulin_resistance/eriks_bonus/data/Pulit-et-al-2018
+wget https://zenodo.org/record/1251813/files/whradjbmi.giant-ukbb.meta-analysis.males.23May2018.txt.gz -P /users/mgloud/projects/insulin_resistance/eriks_bonus/data/Pulit-et-al-2018
---
+++ scripts/new_analysis/get_quantiles.R
@@ -0,0 +1,26 @@
+require(qvalue)
+
+results = read.csv("/users/mgloud/projects/insulin_resistance/output/full_coloc_results_qced.txt", sep=",")
+results$gwas_p = 10^(-results$X.log_gwas_pval)
+results$eqtl_p = 10^(-results$X.log_eqtl_pval)
+
+# Quantiles for various cutoffs
+quantile(results$clpp_mod, seq(0,1,by=0.05))
+quantile(results[(results$X.log_gwas_pval > -log10(5e-8)) & (results$X.log_eqtl_pval > -log10(1e-5)),]$clpp_mod, seq(0,1,by=0.05))
+quantile(results[(results$X.log_gwas_pval > -log10(1e-5)) & (results$X.log_eqtl_pval > -log10(1e-5)),]$clpp_mod, seq(0,1,by=0.05))
+quantile(results[(results$X.log_gwas_pval > -log10(5e-8)) & (results$X.log_eqtl_pval > -log10(1e-6)),]$clpp_mod, seq(0,1,by=0.05))
+
+filtered = results[(results$X.log_gwas_pval > -log10(5e-8)) & (results$X.log_eqtl_pval > -log10(1e-6)),]
+
+# Histogram of values
+hist(1-results$clpp)
+hist(1-results$clpp_mod)
+
+hist(1-filtered$clpp)
+hist(1-filtered$clpp_mod)
+
+qvalue(p=1-results$clpp)
+qvalue(p=1-results$clpp_mod)
+qvalue(p=1-filtered$clpp)
+qvalue(p=1-filtered$clpp_mod)
+
