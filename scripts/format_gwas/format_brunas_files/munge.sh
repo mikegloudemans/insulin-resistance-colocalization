d5baf84d23306b9429ddcaf0fb3a705b1b5c91ae
mikegloudemans <mgloud@durga.stanford.edu>
2019-08-22 11:33:53 -0700

    Combined all munging for this project into a single script

--- scripts/format_gwas/format_brunas_files/munge.sh
+++
@@ -1,2 +0,0 @@
-python /users/mgloud/projects/gwas/scripts/custom_munge.py munge_brunas_files.config
-python /users/mgloud/projects/gwas/scripts/custom_munge.py munge_bonus_files.config
---
+++ scripts/format_gwas/munge.sh
@@ -0,0 +1,1 @@
+python /users/mgloud/projects/gwas-download/scripts/custom_munge.py munge_all.config
