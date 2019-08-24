adf41e7d0146c74cfb0280b9c01a2294d8312b4c
mikegloudemans <mgloud@durga.stanford.edu>
2019-08-24 16:27:13 -0700

    Got it running with GTEx v8

--- scripts/pipe.sh
+++ scripts/pipe.sh
@@ -1,3 +1,14 @@
 
 # Munge all files of interest
 bash /users/mgloud/projects/insulin_resistance/scripts/format_gwas/munge.sh
+
+# List SNPs to test
+python /users/mgloud/projects/gwas-download/overlap/list_snps_to_test.py /users/mgloud/projects/insulin_resistance/scripts/config/ir.overlap.config
+
+# Run colocalization pipeline
+python colocalization/test_IR_colocs.py
+
+# Make table with colocalization results
+
+# Bin coloc results into different categories of loci
+# based on Ivan's method
