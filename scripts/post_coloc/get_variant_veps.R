62517fcce0f57c84939671dec83cb94b88edc378
mikegloudemans <mgloud@durga.stanford.edu>
2020-02-11 10:44:13 -0800

    Commented VEPs file, fixed a memory issue

--- scripts/post_coloc/get_variant_veps.R
+++ scripts/post_coloc/get_variant_veps.R
@@ -7,7 +7,9 @@ require(rjson)
 # For each locus, make a list of possible causal variants (based on LD with lead variants or CLPP-mod threshold or something)
 # and for each, list the variant's predicted effect from VEP
 
+# CADD file, which contains VEP consequence annotations, the field we're looking for
 cadd_file = "data/cadd/whole_genome_SNVs_inclAnno.tsv.gz"
+# LD file, which contains all SNP pairs with r^2 >= 0.8
 ld_file = "data/ld/EUR_geno_hg38.txt.gz"
 
 #####################################################
@@ -25,12 +27,13 @@ colocs = read.table(paste0(config$out_dir, "/clpp_results_categorized_", config$
 ### Get list of variants to annotate
 #####################################################
 
+# Figure out which columns of the LD file we need
 ld_head = unlist(strsplit(unlist(system(sprintf("zcat %s | head -n 1", ld_file), intern=TRUE)), "\t"))
 ld_r2 = which(ld_head == "ld_r2")
 ld_chr = which(ld_head == "snp2_chrom_hg38")
 ld_pos = which(ld_head == "snp2_pos_hg38")
 
-
+# Figure out which columns of the CADD file we need, to get the VEP consequence field
 head = system(sprintf("zcat %s | tail -n +2 | head -n 1", cadd_file), intern=TRUE)
 head = unlist(strsplit(head, "\t"))
 vep_index = which(head == "Consequence")
@@ -39,14 +42,16 @@ alt_index = which(head == "Alt")
 gene_index = which(head == "GeneID")
 feature_index = which(head == "FeatureID")
 
-max_num_loci = 100000
-all_loci = as.data.frame(list(chr=rep(0, max_num_loci), pos=rep(0, max_num_loci), locus=rep(0, max_num_loci), vep = rep("", max_num_loci), ref=rep("", max_num_loci), alt=rep("", max_num_loci), gene=rep("", max_num_loci), feature=rep("", max_num_loci), r2=rep("", max_num_loci)), stringsAsFactors=FALSE)
+# Initialize a data frame to hold all relevant SNPs
+max_total_snps = 1000000
+all_loci = as.data.frame(list(chr=rep(0, max_total_snps), pos=rep(0, max_total_snps), locus=rep(0, max_total_snps), vep = rep("", max_total_snps), ref=rep("", max_total_snps), alt=rep("", max_total_snps), gene=rep("", max_total_snps), feature=rep("", max_total_snps), r2=rep("", max_total_snps)), stringsAsFactors=FALSE)
+
 loc_table_idx = 0
 # For each locus
 for (l in 1:max(colocs$locus))
 {
 	print(l)
-	# Get all lead SNPs for that locus
+	# Get all lead SNPs for that locus (regardless of GWAS trait)
 	locus_colocs = colocs %>% filter(locus == l) %>% select(chr, pos)
 	
 	locus_snps = locus_colocs %>% filter(!duplicated(locus_colocs))
@@ -55,19 +60,20 @@ for (l in 1:max(colocs$locus))
 	max_num_snps = 100000	# Couldn't possibly be more than this could it?
 	all_snps = as.data.frame(list(chr=rep(0, max_num_snps), pos=rep(0, max_num_snps), ld=rep(0, max_num_snps)))
 
-	# For each lead SNP
 	snp_num = 0
-
+	# For each lead SNP, get its LD buddies
 	for (snp in 1:dim(locus_snps)[1])
 	{	
 		snp_num = snp_num + 1
+		
+		# Add the lead SNP to the list
 		all_snps$chr[snp_num] = locus_snps$chr[snp]
 		all_snps$pos[snp_num] = locus_snps$pos[snp]
 		all_snps$ld[snp_num] = 1
 			
-		# NOTE: This isn't perfect but it's fine, will do the job for now
-		# TODO: Get all SNPs that are LD buddies of any of those lead SNPs (>0.8) and add them to a "set" for the locus
-		ld = unlist(system(sprintf("tabix %s chr%s:%s-%s", ld_file, as.character(locus_snps$chr[snp]), as.character(locus_snps$pos[snp]), as.character(locus_snps$pos[snp])), intern=TRUE))
+		# Get all SNPs that are LD buddies of any of those lead SNPs (r2>0.8) and add them to a "set" for the locus
+		ld = unlist(system(sprintf("tabix %s chr%s:%s-%s", ld_file, as.character(locus_snps$chr[snp]), 
+					   as.character(locus_snps$pos[snp]), as.character(locus_snps$pos[snp])), intern=TRUE))
 		if (length(ld)==0)
 		{
 			next
@@ -81,13 +87,18 @@ for (l in 1:max(colocs$locus))
 			all_snps$ld[snp_num] = buddy[ld_r2]
 		}
 	}
+	# Remove duplicates, since some SNPs might have been added more than once.
+	# Keep the one with the highest LD with a lead GWAS SNP
 	all_snps = all_snps[1:snp_num,]
 	all_snps = all_snps %>% arrange(desc(ld))
 	all_snps = all_snps %>% filter(!duplicated(paste(chr, pos)))
 
+	# Now that we have all the SNPs and their LD buddies,
+	# get VEP consequence annotations for the final table
 	for (snp in 1:dim(all_snps)[1])
 	{
-		out = unlist(system(sprintf("tabix %s %s:%s-%s", cadd_file, as.character(all_snps$chr[snp]), as.character(all_snps$pos[snp]), as.character(all_snps$pos[snp])), intern=TRUE))
+		out = unlist(system(sprintf("tabix %s %s:%s-%s", cadd_file, as.character(all_snps$chr[snp]), 
+					    as.character(all_snps$pos[snp]), as.character(all_snps$pos[snp])), intern=TRUE))
 		for (o in 1:length(out))
 		{
 			loc_table_idx = loc_table_idx + 1
@@ -115,5 +126,10 @@ all_loci = all_loci[1:loc_table_idx,]
 all_loci$id = paste(all_loci$vep, all_loci$chr, all_loci$pos, all_loci$ref, all_loci$alt, all_loci$gene, all_loci$feature, all_loci$r2, sep=".")
 vep_summary = all_loci %>% group_by(locus) %>% summarize(paste(id, collapse="|"))
 
+# Write output files
+
+# One row per variant
+write.table(all_loci, sep="\t", row.names=FALSE, col.names=TRUE, quote=FALSE, file=paste0(config$out_dir, "/vep_summary_melted_", config$analysis_date, ".txt"))
+# One row per locus
 write.table(vep_summary, sep="\t", row.names=FALSE, col.names=TRUE, quote=FALSE, file=paste0(config$out_dir, "/vep_summary_", config$analysis_date, ".txt"))
 
